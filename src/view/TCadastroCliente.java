a6466b9e0f12464d7c6900843bd5e7c6d4fd1377
nando <nandogoulart.m@gmail.com>
2023-05-15 23:21:21 -0300

    fix(icon): cadastro

--- src/view/TCadastroCliente.java
+++ src/view/TCadastroCliente.java
@@ -92,7 +92,7 @@ public class TCadastroCliente extends javax.swing.JDialog {
         jPanel5.setBackground(new java.awt.Color(153, 153, 153));
         jPanel5.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view.icons/Create.png"))); // NOI18N
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/icons/Create.png"))); // NOI18N
         jButton1.setText("Novo");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -101,19 +101,19 @@ public class TCadastroCliente extends javax.swing.JDialog {
         });
         jPanel5.add(jButton1);
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view.icons/Erase.png"))); // NOI18N
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/icons/Erase.png"))); // NOI18N
         jButton2.setText("Cancelar");
         jPanel5.add(jButton2);
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view.icons/OK.png"))); // NOI18N
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/icons/OK.png"))); // NOI18N
         jButton3.setText("Gravar");
         jPanel5.add(jButton3);
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view.icons/Find.png"))); // NOI18N
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/icons/Find.png"))); // NOI18N
         jButton4.setText("Buscar");
         jPanel5.add(jButton4);
 
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view.icons/Exit.png"))); // NOI18N
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/icons/Exit.png"))); // NOI18N
         jButton5.setText("Sair");
         jPanel5.add(jButton5);
 
@@ -398,74 +398,6 @@ public class TCadastroCliente extends javax.swing.JDialog {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLTitulo;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel15;
-    private javax.swing.JLabel jLabel16;
-    private javax.swing.JLabel jLabel17;
-    private javax.swing.JLabel jLabel18;
-    private javax.swing.JLabel jLabel19;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel20;
-    private javax.swing.JLabel jLabel21;
-    private javax.swing.JLabel jLabel22;
-    private javax.swing.JLabel jLabel23;
-    private javax.swing.JLabel jLabel24;
-    private javax.swing.JLabel jLabel25;
-    private javax.swing.JLabel jLabel26;
-    private javax.swing.JLabel jLabel27;
-    private javax.swing.JLabel jLabel28;
-    private javax.swing.JLabel jLabel29;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel30;
-    private javax.swing.JLabel jLabel31;
-    private javax.swing.JLabel jLabel32;
-    private javax.swing.JLabel jLabel33;
-    private javax.swing.JLabel jLabel34;
-    private javax.swing.JLabel jLabel35;
-    private javax.swing.JLabel jLabel36;
-    private javax.swing.JLabel jLabel37;
-    private javax.swing.JLabel jLabel38;
-    private javax.swing.JLabel jLabel39;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel40;
-    private javax.swing.JLabel jLabel41;
-    private javax.swing.JLabel jLabel42;
-    private javax.swing.JLabel jLabel43;
-    private javax.swing.JLabel jLabel44;
-    private javax.swing.JLabel jLabel45;
-    private javax.swing.JLabel jLabel46;
-    private javax.swing.JLabel jLabel47;
-    private javax.swing.JLabel jLabel48;
-    private javax.swing.JLabel jLabel49;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel50;
-    private javax.swing.JLabel jLabel51;
-    private javax.swing.JLabel jLabel52;
-    private javax.swing.JLabel jLabel53;
-    private javax.swing.JLabel jLabel54;
-    private javax.swing.JLabel jLabel55;
-    private javax.swing.JLabel jLabel56;
-    private javax.swing.JLabel jLabel57;
-    private javax.swing.JLabel jLabel58;
-    private javax.swing.JLabel jLabel59;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel60;
-    private javax.swing.JLabel jLabel61;
-    private javax.swing.JLabel jLabel62;
-    private javax.swing.JLabel jLabel63;
-    private javax.swing.JLabel jLabel64;
-    private javax.swing.JLabel jLabel65;
-    private javax.swing.JLabel jLabel66;
-    private javax.swing.JLabel jLabel67;
-    private javax.swing.JLabel jLabel68;
-    private javax.swing.JLabel jLabel69;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel70;
     private javax.swing.JLabel jLabel71;
     private javax.swing.JLabel jLabel72;
     private javax.swing.JLabel jLabel73;
@@ -475,100 +407,21 @@ public class TCadastroCliente extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel77;
     private javax.swing.JLabel jLabel78;
     private javax.swing.JLabel jLabel79;
-    private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel80;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPCadastro;
     private javax.swing.JPanel jPTitulo;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel10;
-    private javax.swing.JPanel jPanel11;
     private javax.swing.JPanel jPanel12;
-    private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JPanel jPanel7;
-    private javax.swing.JPanel jPanel8;
-    private javax.swing.JPanel jPanel9;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField10;
-    private javax.swing.JTextField jTextField11;
-    private javax.swing.JTextField jTextField12;
-    private javax.swing.JTextField jTextField13;
-    private javax.swing.JTextField jTextField14;
-    private javax.swing.JTextField jTextField15;
-    private javax.swing.JTextField jTextField16;
-    private javax.swing.JTextField jTextField17;
-    private javax.swing.JTextField jTextField18;
-    private javax.swing.JTextField jTextField19;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField20;
-    private javax.swing.JTextField jTextField21;
     private javax.swing.JTextField jTextField22;
     private javax.swing.JTextField jTextField23;
     private javax.swing.JTextField jTextField24;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField7;
-    private javax.swing.JTextField jTextField8;
-    private javax.swing.JTextField jTextField9;
-    private javax.swing.JFormattedTextField txtAniver;
-    private javax.swing.JFormattedTextField txtAniver1;
-    private javax.swing.JFormattedTextField txtAniver2;
-    private javax.swing.JFormattedTextField txtAniver3;
-    private javax.swing.JFormattedTextField txtAniver4;
-    private javax.swing.JFormattedTextField txtAniver5;
-    private javax.swing.JFormattedTextField txtAniver6;
     private javax.swing.JFormattedTextField txtAniver7;
-    private javax.swing.JFormattedTextField txtCEP;
-    private javax.swing.JFormattedTextField txtCEP1;
-    private javax.swing.JFormattedTextField txtCEP2;
-    private javax.swing.JFormattedTextField txtCEP3;
-    private javax.swing.JFormattedTextField txtCEP4;
-    private javax.swing.JFormattedTextField txtCEP5;
-    private javax.swing.JFormattedTextField txtCEP6;
     private javax.swing.JFormattedTextField txtCEP7;
-    private javax.swing.JFormattedTextField txtCPF;
-    private javax.swing.JFormattedTextField txtCPF1;
-    private javax.swing.JFormattedTextField txtCPF2;
-    private javax.swing.JFormattedTextField txtCPF3;
-    private javax.swing.JFormattedTextField txtCPF4;
-    private javax.swing.JFormattedTextField txtCPF5;
-    private javax.swing.JFormattedTextField txtCPF6;
     private javax.swing.JFormattedTextField txtCPF7;
-    private javax.swing.JTextField txtCidade;
-    private javax.swing.JTextField txtCidade1;
-    private javax.swing.JTextField txtCidade10;
-    private javax.swing.JTextField txtCidade11;
-    private javax.swing.JTextField txtCidade12;
-    private javax.swing.JTextField txtCidade13;
     private javax.swing.JTextField txtCidade14;
     private javax.swing.JTextField txtCidade15;
-    private javax.swing.JTextField txtCidade2;
-    private javax.swing.JTextField txtCidade3;
-    private javax.swing.JTextField txtCidade4;
-    private javax.swing.JTextField txtCidade5;
-    private javax.swing.JTextField txtCidade6;
-    private javax.swing.JTextField txtCidade7;
-    private javax.swing.JTextField txtCidade8;
-    private javax.swing.JTextField txtCidade9;
-    private javax.swing.JFormattedTextField txtFone1;
-    private javax.swing.JFormattedTextField txtFone10;
-    private javax.swing.JFormattedTextField txtFone11;
-    private javax.swing.JFormattedTextField txtFone12;
-    private javax.swing.JFormattedTextField txtFone13;
-    private javax.swing.JFormattedTextField txtFone14;
     private javax.swing.JFormattedTextField txtFone15;
     private javax.swing.JFormattedTextField txtFone16;
-    private javax.swing.JFormattedTextField txtFone2;
-    private javax.swing.JFormattedTextField txtFone3;
-    private javax.swing.JFormattedTextField txtFone4;
-    private javax.swing.JFormattedTextField txtFone5;
-    private javax.swing.JFormattedTextField txtFone6;
-    private javax.swing.JFormattedTextField txtFone7;
-    private javax.swing.JFormattedTextField txtFone8;
-    private javax.swing.JFormattedTextField txtFone9;
     // End of variables declaration//GEN-END:variables
 }
